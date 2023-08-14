dda3b301cba25ebefa1779399a76b99911ccf3a8
babycode <babycode.2023@gmail.com>
2023-08-14 09:37:53 +0800

    setup component

--- little-lemon/src/components/Navbar/Navbar.jsx
+++ little-lemon/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { images } from '../../contants';
+import "./Navbar.scss"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {['HOME', 'ABOUT', 'MENU', 'RESERVATIONS', 'ORDER OUTLINE','LOGIN'].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
